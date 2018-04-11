aa31f264d37bc6dbfa5bb49ce445afd97359d1c7
Sean Lightfoot <seanalexander360@gmail.com>
2018-04-10 23:19:06 -0400

    update

---
+++ frameworkExercise/frameworkExercise/week11/js/init.js
@@ -0,0 +1,14 @@
+(function ($) {
+  $(function () {
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})
+
+$(document).ready(function () {
+  $('.sidenav').sidenav();
+})
+
+
+(jQuery); // end of jQuery name space
